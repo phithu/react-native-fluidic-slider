2fce27d9beb8a59801e226962eab911ebc9fdba6
phithu <phithu.dev@gmail.com>
2020-12-09 13:37:54 +0700

    Fix

--- ios/Pods/fluid-slider/Sources/ValueView.swift
+++ ios/Pods/fluid-slider/Sources/ValueView.swift
@@ -12,52 +12,52 @@ import pop
 class ValueView : UIView {
 
     static let kLayoutMarginInset: CGFloat = 4
-    
+
     // MARK: - Initialization
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         initialize()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initialize()
     }
-    
+
     private func initialize() {
         shapeView.frame = bounds
         shapeView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         shapeView.layer.addSublayer(outerShapeLayer)
         shapeView.layer.addSublayer(innerShapeLayer)
         addSubview(shapeView)
-        
+
         shapeView.addSubview(textLabel)
     }
-    
+
     // MARK: - Layers
-    
+
     private let outerShapeLayer = CAShapeLayer()
     private let innerShapeLayer = CAShapeLayer()
-    
+
     var outerFillColor: UIColor? {
         didSet {
             outerShapeLayer.fillColor = outerFillColor?.cgColor
             outerShapeLayer.removeAllAnimations()
         }
     }
-    
+
     var innerFillColor: UIColor? {
         didSet {
             innerShapeLayer.fillColor = innerFillColor?.cgColor
             innerShapeLayer.removeAllAnimations()
         }
     }
-    
+
     // MARK: - Text Label
-    
+
     private let textLabel = UILabel()
-    
+
     var attributedText: NSAttributedString? {
         get {
             return textLabel.attributedText
@@ -70,33 +70,32 @@ class ValueView : UIView {
 				paragraph.alignment = .center
 				string.addAttribute(.paragraphStyle, value: paragraph, range: NSMakeRange(0, string.length))
 				textLabel.attributedText = string
-                textLabel.textColor = UIColor.white
 			} else {
 				textLabel.attributedText = nil
 			}
 		}
     }
-    
+
     // MARK: - Laying out Subviews
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+
         textLabel.frame = bounds
-        
+
         outerShapeLayer.path = UIBezierPath(ovalIn: shapeView.bounds).cgPath
         innerShapeLayer.path = UIBezierPath(ovalIn: shapeView.bounds.insetBy(dx: ValueView.kLayoutMarginInset, dy: ValueView.kLayoutMarginInset)).cgPath
         layer.removeAllAnimations()
     }
-    
+
     // MARK: - Animations
-    
+
     private let shapeView = UIView()
     public var animationFrame: (() -> ())?
-    
+
     func animateTrackingBegin() {
         let topY = -shapeView.bounds.height - 4
-        
+
         if let animation = POPSpringAnimation(propertyNamed: kPOPLayerPositionY) {
             animation.toValue = topY + shapeView.bounds.midY
             animation.springBounciness = 8
@@ -108,7 +107,7 @@ class ValueView : UIView {
             shapeView.layer.pop_add(animation, forKey: "bounce")
         }
     }
-    
+
     func animateTrackingEnd() {
         if let animation = POPBasicAnimation(propertyNamed: kPOPLayerPositionY) {
             animation.toValue = shapeView.bounds.midY
@@ -120,5 +119,5 @@ class ValueView : UIView {
             shapeView.layer.pop_add(animation, forKey: "bounce")
         }
     }
-    
+
 }
